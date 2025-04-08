db77cda9e6d6b5225f0a2669c50c3382b5770294
Yingjie <yingjieqi@buaa.edu.cn>
2025-04-08 13:48:06 +0800

    Update metadata

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -4,8 +4,8 @@ import { baseOptions } from '@/app/layout.config';
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'CIMFlow - Integrated Framework for CIM Design',
-  description: 'CIMFlow is an integrated framework for the systematic design, compilation, and simulation of SRAM-based digital compute-in-memory (CIM) architectures. It supports ISA abstraction, compiler mapping, and cycle-accurate SystemC simulation.',
+  title: 'CIMFlow - An Integrated Framework for CIM Design',
+  description: 'CIMFlow is an integrated framework for the systematic design, compilation, and simulation of SRAM-based digital compute-in-memory (CIM) architectures. It supports ISA abstraction, compilation optimization, and cycle-accurate SystemC simulation.',
 }
 
 export default function Layout({ children }: { children: ReactNode }) {
